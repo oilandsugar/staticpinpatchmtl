932503d4b344e07ee8f3d3249e17a83860cb2c42
oilandsugar <tommy_pm@hotmail.com>
2019-02-09 19:58:03 -0500

    add slider artists

--- themes/pinpatchmtl/static/js/script.js
+++ themes/pinpatchmtl/static/js/script.js
@@ -28,20 +28,6 @@ jQuery(function($) {
       feed.run()
     }
 
-    // send apply email
-
-    $("#apply-form").submit(function(event) {
-      event.preventDefault()
-      var shop_name = $("#apply-shop-name").val()
-      var shop_link = $("#apply-shop-name").val()
-      var contact_name = $("#apply-contact-name").val()
-      var contact_email = $("#apply-contact-email").val()
-      var hometown = $("#apply-hometown").val()
-      var notes = $("#apply-notes").val()
-
-      console.log("apply form sent")
-    })
-
     // mobile menu
 
     $('.menu-button').click(function() {
@@ -87,48 +73,85 @@ jQuery(function($) {
         }
     })
 
-/*
-    var i18n = window.domI18n({
-      selector: "[data-translatable]",
-      separator: " // ",
-      languages: ["en", "fr"],
-      defaultLanguage: "en",
-      translatableAttr: "value"
-    })
+    // when images are loaded
 
+        $(".hover-slider").each(function() {
+          let slider = $(this);
+          slider.find('.img').imagesLoaded({ background: true }, function(instance, image) {
+            slider.find('.images-container').removeClass('not-loaded');
+          });
+        });
 
+    // hover slider on photo based projects
 
-    var userLang = null
-    if (navigator.language) {
-      if (navigator.language.indexOf("en") !== -1) {
-        userLang = "en"
-        $("#en-link").addClass("active")
-        $("#fr-link").removeClass("active")
-      } else if (navigator.language.indexOf("fr") !== -1) {
-        userLang = "fr"
-        $("#fr-link").addClass("active")
-        $("#en-link").removeClass("active")
+    let sliderWidth = $('.hover-slider').width();
+
+    $(".hover-slider").each(function() {
+      let slider = $(this);
+      let images = $(this).find('.img');
+      let indicator = $(this).find('.indicator');
+      let container = $(this).find('.images-container');
+      // only initialize if there is more than one image
+      if(images.length > 1) {
+        // set width of container
+        let containerWidth = slider.width() * images.length;
+        container.css('width', containerWidth + "px");
+        // set width of images inside
+        slider.find('.img').css('width', sliderWidth + "px");
+        // position images
+        slider.find('#image-2').css('left', sliderWidth + "px");
+        slider.find('#image-3').css('left', sliderWidth * 2 + "px");
+        // add indicator for each image
+        $.each(images, function(key, value) {
+          if(key == 0) {
+            indicator.append('<div class="bar active"></div>');
+          } else {
+            indicator.append('<div class="bar"></div>');
+          }
+        });
       }
-    } else if (navigator.userLanguage) {
-      if (navigator.userLanguage.indexOf("en") !== -1) {
-        userLang = "en"
-        $("#en-link").addClass("active")
-        $("#fr-link").removeClass("active")
-      } else if (navigator.userLanguage.indexOf("fr") !== -1) {
-        userLang = "fr"
-        $("#fr-link").addClass("active")
-        $("#en-link").removeClass("active")
+    });
+
+    $(".hover-slider").mousemove(function(e) {
+      let slider = $(this);
+      let images = $(this).find('.img');
+      let container = slider.find(".images-container");
+      if(images.length > 1) {
+        let parentOffset = slider.offset();
+        var relX = e.pageX - parentOffset.left;
+        let portionWidth = sliderWidth / images.length;
+        // define positions to move slider
+        // move slider to show image 1
+        if(relX < portionWidth){
+          container.css('left', 0);
+          slider.find('.bar').removeClass('active');
+          slider.find('.bar:nth-child(1)').addClass('active');
+        }
+        // move slider to show image 2
+        if(relX >= portionWidth && relX < portionWidth * 2) {
+          container.css('left', "-" + sliderWidth + "px");
+          slider.find('.bar').removeClass('active');
+          slider.find('.bar:nth-child(2)').addClass('active');
+        }
+        // move slider to show image 3
+        if(images.length == 3 && relX >= portionWidth * 2){
+          container.css('left', "-" + sliderWidth * 2 + "px");
+          slider.find('.bar').removeClass('active');
+          slider.find('.bar:nth-child(3)').addClass('active');
+        }
       }
-    } else {
-      userLang = "en"
-      $("#en-link").addClass("active")
-      $("#fr-link").removeClass("active")
-    }
+    });
 
-    if (userLang) {
-      i18n.changeLanguage(userLang)
-    }*/
-
+    $(".hover-slider").mouseout(function() {
+      let slider = $(this);
+      let images = $(this).find('.img');
+      let container = slider.find(".images-container");
+      if(images.length > 1) {
+        container.css('left', 0);
+        slider.find('.bar').removeClass('active');
+        slider.find('.bar:nth-child(1)').addClass('active');
+      }
+    });
 
     // draggable thingy
 
